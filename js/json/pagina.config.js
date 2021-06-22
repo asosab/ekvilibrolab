af8e09415e9eae2e51babc12639eb532709e7cf8
Alejandro Sosa Briceño <asosab@gmail.com>
2021-06-22 19:44:12 -0400

    Update pagina.config.js

--- js/json/pagina.config.js
+++ js/json/pagina.config.js
@@ -5,23 +5,13 @@ pagina.config = {
 //		,{'t':'Galeria','u':'#p=galeria','c':''} 
 //		,{'t':'Presupuesto','u':'#p=presupuesto','c':''}
 
-//		,{'t':'Lab','u':'#','c':'dropdown','l':[
-//			{'t':'Uml','u':'#p=uml','c':''}
-//			,{'t':'Flowchart','u':'#p=flowchart','c':''}
-//			,{'t':'Academico','u':'#p=academico','c':''}
-//			,{'t':'Jspdf','u':'#p=jspdf','c':''}
-//			,{'t':'Chart','u':'#p=chart','c':''}
-//			,{'t':'','u':'','c':'divider'}
-//			,{'t':'Pruebas','u':'','c':'dropdown-header'}
-//			,{'t':'Galeria2','u':'#p=galeria2','c':''}
-//			,{'t':'Test','u':'#p=test','c':''}
-//			,{'t':'Facedetect','u':'#p=facedetect','c':''}
-//			,{'t':'Html2canvas','u':'#p=html2canvas','c':''}
-//			,{'t':'Personas','u':'#p=personas','c':''}
-//			,{'t':'Fileupload','u':'#p=fileupload','c':''}
-//			
-//			]
-//		} 
+		,{'t':'Kombuchas','u':'#','c':'dropdown','l':[
+			{'t':'Flor de Jamaica','u':'#p=KJ3','c':''}
+			,{'t':'Jengibre, cúrcuma y miel','u':'#p=KJC3','c':''}
+			]
+		} 
+
+
 	]
 	,'menur':[
 //		{'t':'Contacto','u':'#a=contacto','c':''} 
