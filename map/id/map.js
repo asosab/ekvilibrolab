9fee16bbafe5581424250200e9b7cbf5977f3ffd
Alejandro Sosa <asosab@gmail.com>
2024-03-24 13:37:31 -0400

    inmuebles

---
+++ map/id/map.js
@@ -0,0 +1,75 @@
+var map;
+var locations = [];
+
+function initialiseMap() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var id = urlParams.get('id');
+  var key = urlParams.get('key'); //'AIzaSyCz2nyy2FyAGVGODM3rqOlDD_bVzwC4iVw';
+//  var gid = '661544739';
+  var valores = 'Sheet1!A2:F';
+  var url = 'https://sheets.googleapis.com/v4/spreadsheets/'+id+'/values/'+valores+'?key='+key;
+  $.getJSON(url, function(data) {
+    	$(data.values).each(function() {
+    		var location = {};
+				location.nombre = this[0];
+				location.lat = parseFloat(this[1]);
+      	location.lon = parseFloat(this[2]);
+       	location.dir = this[3];
+        location.URL = this[4];
+        location.des = this[5];
+	  		locations.push(location);
+    	});
+
+      var mapOptions = {
+        zoom: 11,
+        center: new google.maps.LatLng(0, 0)
+      };
+      var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      setLocations(map, locations);
+  });
+}
+
+
+function setLocations(map, locations) {
+  var bounds = new google.maps.LatLngBounds();
+  var infowindow = new google.maps.InfoWindow({content: "Content String"});
+  for (var i = 0; i < locations.length; i++) {
+    var new_marker = createMarker(map, locations[i], infowindow);
+    bounds.extend(new_marker.position);
+  }
+  map.fitBounds(bounds);
+}
+
+function iconoMapa(tipo,rank){ 
+  var i = 1;
+  var icon = (tipo ==1)?"tienda0":"person0"; 
+  if (rank>500)   {i = 2;}
+  if (rank>1000)  {i = 3;}
+  if (rank>10000) {i = 4;}
+  return "https://ekvilibrolab.com/imagen/icons/"+icon+i+".png";
+}
+
+function createMarker(map, location, infowindow) {
+  var position = {
+    lat: parseFloat(location.lat),
+    lng: parseFloat(location.lon)
+  };
+
+  var marker = new google.maps.Marker({
+    position: position,
+    map: map,
+    title: location.nombre,
+//    icon: iconoMapa(location.tie,location.ran)
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent('<div>'+
+//    '<p><strong>' + 
+    ((location.nombre   === undefined) ? "" : ('<p><strong>' + location.nombre + '</strong></p>')) +
+    ((location.dir      === undefined) ? "" : ('<p><strong>Direccion: </strong>' + location.dir + '</p>')) +
+    ((location.url      === undefined) ? "" : ('<p><a href="'+location.url+'">Web</a><br/></p>')) +
+    ((location.des      === undefined) ? "" : ('<p>'+location.des+'<br/></p>')) +
+    '</div>');
+    infowindow.open(map, marker);
+  });
+  return marker;
+}
