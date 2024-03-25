20fd22ac9943a806f14e61d0db1a30641b485c43
Alejandro Sosa <asosab@gmail.com>
2024-03-24 21:26:15 -0400

    Update map.js

--- map/id/map.js
+++ map/id/map.js
@@ -1,10 +1,10 @@
 var map;
 var locations = [];
 
-async function initialiseMap() {
+async function initialiseMap2() {
   var urlParams = new URLSearchParams(window.location.search);
-  let id = d((urlParams.get('id'))); //urlParams.get('id');
-  let key = d((urlParams.get('key'))); //urlParams.get('key');
+  let id = desencriptar((urlParams.get('id'))); //urlParams.get('id');
+  let key = desencriptar((urlParams.get('key'))); //urlParams.get('key');
 
 //  var gid = '661544739';
   var valores = 'Sheet1!A2:F';
@@ -29,6 +29,47 @@ async function initialiseMap() {
       setLocations(map, locations);
   });
 }
+
+async function initialiseMap() {
+  try {
+    var urlParams = new URLSearchParams(window.location.search);
+    let id = urlParams.get('id');
+    let key = urlParams.get('key');
+
+    if (!id || !key) {
+      throw new Error("ID o clave no proporcionados en la URL");
+    }
+
+    id = await desencriptar(id);
+    key = await desencriptar(key);
+
+    var valores = 'Sheet1!A2:F';
+    var url = 'https://sheets.googleapis.com/v4/spreadsheets/' + id + '/values/' + valores + '?key=' + key;
+
+    $.getJSON(url, function (data) {
+      $(data.values).each(function () {
+        var location = {};
+        location.nombre = this[0];
+        location.lat = parseFloat(this[1]);
+        location.lon = parseFloat(this[2]);
+        location.dir = this[3];
+        location.URL = this[4];
+        location.des = this[5];
+        locations.push(location);
+      });
+
+      var mapOptions = {
+        zoom: 11,
+        center: new google.maps.LatLng(0, 0)
+      };
+      var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      setLocations(map, locations);
+    });
+  } catch (error) {
+    console.error("Error al inicializar el mapa:", error);
+  }
+}
+
 
 
 function setLocations(map, locations) {
@@ -75,12 +116,17 @@ function createMarker(map, location, infowindow) {
   return marker;
 }
 
-function d(fraseEncriptada) {
+async function desencriptar(fraseEncriptada) {
+  const CARACTERES_ALFANUMERICOS = 36;
+  const CARACTERES_NUMERICOS_INICIALES = 48;
+  const CARACTERES_MINUSCULAS_INICIALES = 87;
+  const CAMBIO_CLAVE_CADA_HORAS = 2;
+
   const horaActual = new Date().getHours();
-  const clave = Math.floor(horaActual / 2); // La clave cambia cada dos horas
+  const clave = Math.floor(horaActual / CAMBIO_CLAVE_CADA_HORAS);
   const fraseDesencriptada = fraseEncriptada.split('').map(caracter => {
     const codigoAscii = caracter.charCodeAt(0);
-    const codigoDesencriptado = (codigoAscii - (codigoAscii < 58 ? 48 : 87) - clave + 36) % 36; // Convertir de nuevo a ASCII
+    const codigoDesencriptado = (codigoAscii - (codigoAscii < 58 ? CARACTERES_NUMERICOS_INICIALES : CARACTERES_MINUSCULAS_INICIALES) - clave + CARACTERES_ALFANUMERICOS) % CARACTERES_ALFANUMERICOS;
     return String.fromCharCode(codigoDesencriptado);
   }).join('');
   return fraseDesencriptada;
